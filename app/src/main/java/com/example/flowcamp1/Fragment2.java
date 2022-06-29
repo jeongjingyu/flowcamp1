20e90b68d2f05ccaa87490fbc754ed494f2e5b5a
yeonyeonn <wuvele22@g.skku.edu>
2022-06-29 22:25:55 +0900

    fragment1 (adding listview)

--- app/src/main/java/com/example/flowcamp1/Fragment2.java
+++ app/src/main/java/com/example/flowcamp1/Fragment2.java
@@ -18,6 +18,6 @@ public class Fragment2 extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment2, container, false)
+        return inflater.inflate(R.layout.fragment2, container, false);
     }
 }
