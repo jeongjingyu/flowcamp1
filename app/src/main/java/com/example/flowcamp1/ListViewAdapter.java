2544a27faff90b920251dcd2141c3c556344b643
yeonyeonn <wuvele22@g.skku.edu>
2022-06-30 15:16:04 +0900

    fragment1 expandable list view

--- app/src/main/java/com/example/flowcamp1/ListViewAdapter.java
+++ app/src/main/java/com/example/flowcamp1/ListViewAdapter.java
@@ -7,23 +7,31 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ListAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
-public class ListViewAdapter extends BaseAdapter implements AdapterView.OnItemClickListener {
+public class ListViewAdapter extends BaseExpandableListAdapter {
     private Context context;
     private ArrayList<list_form> items;
+    private HashMap<String, String> child;
+    private ViewHolder viewHolder = null;
 
+    public ListViewAdapter(Context context, ArrayList<list_form> items, HashMap<String, String> child) {
+        this.context = context;
+        this.items = items;
+        this.child = child;
+    }
+
+    /*
     public ListViewAdapter(Context context, ArrayList<list_form> items) {
         this.context = context;
         this.items = items;
     }
 
-    @Override
-    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-        Log.d("Clicked", "Clicked");
-    }
 
     @Override
     public int getCount() {
@@ -56,7 +64,91 @@ public class ListViewAdapter extends BaseAdapter implements AdapterView.OnItemCl
         num.setText(items.get(i).num);
 
         return view;
+    } */
+
+
+    @Override
+    public int getGroupCount() {
+        return items.size();
     }
 
+    @Override
+    public int getChildrenCount(int i) {
+        return 1;
+    }
+
+    @Override
+    public Object getGroup(int i) {
+        return items.get(i);
+    }
+
+    @Override
+    public Object getChild(int i, int i1) {
+        return child.get(items.get(i).name);
+    }
+
+    @Override
+    public long getGroupId(int i) {
+        return i;
+    }
+
+    @Override
+    public long getChildId(int i, int i1) {
+        return i1;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    @Override
+    public View getGroupView(int i, boolean b, View view, ViewGroup viewGroup) {
+
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.listview, viewGroup, false);
+        }
+
+        TextView name = (TextView) view.findViewById(R.id.nameTextView);
+        TextView num = (TextView) view.findViewById(R.id.numTextView);
+
+        name.setText(items.get(i).name);
+        num.setText(items.get(i).num);
+
+        return view;
+
+        /*
+        viewHolder = new ViewHolder();
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        view = inflater.inflate(R.layout.listview, viewGroup, false);
+        viewHolder.childNameText = (TextView) view.findViewById(R.id.nameTextView);
+        viewHolder.childnumText = (TextView) view.findViewById(R.id.numTextView);
+        viewHolder = (ViewHolder) view.getTag();
+
+        return view; */
+    }
+
+    @Override
+    public View getChildView(int i, int i1, boolean b, View view, ViewGroup viewGroup) {
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.childlistview, viewGroup, false);
+
+
+        }
+        return view;
+    }
+
+    @Override
+    public boolean isChildSelectable(int i, int i1) {
+        return true;
+    }
+
+    class ViewHolder {
+        public androidx.appcompat.widget.AppCompatButton image;
+        public TextView childNameText;
+        public TextView childnumText;
+    }
 
 }
