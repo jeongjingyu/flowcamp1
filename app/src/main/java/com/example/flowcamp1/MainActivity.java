076ca7c5dff3bbdedde49a03caaa37d1ab02ea6a
jeongjingyu <jjg0516@kaist.ac.kr>
2022-06-30 15:18:37 +0900

    git commit -m 'bottom navigation toolbar implementation'

--- app/src/main/java/com/example/flowcamp1/MainActivity.java
+++ app/src/main/java/com/example/flowcamp1/MainActivity.java
@@ -14,14 +14,13 @@ import com.example.flowcamp1.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
-    Button tab1, tab2, tab3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        replaceFragment(new Fragment1());
+//        replaceFragment(new Fragment1());
 
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
             switch (item.getItemId()) {
@@ -37,39 +36,6 @@ public class MainActivity extends AppCompatActivity {
             }
             return true;
         });
-//        tab1 = (Button)findViewById(R.id.tab1);
-//        tab2 = (Button)findViewById(R.id.tab2);
-//        tab3 = (Button)findViewById(R.id.tab3);
-//
-//        tab1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-//                Fragment1 fragment1 = new Fragment1();
-//                transaction.replace(R.id.frame, fragment1);
-//                transaction.commit();
-//            }
-//        });
-//
-//        tab2.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-//                Fragment2 fragment2 = new Fragment2();
-//                transaction.replace(R.id.frame, fragment2);
-//                transaction.commit();
-//            }
-//        });
-//
-//        tab3.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-//                Fragment3 fragment3 = new Fragment3();
-//                transaction.replace(R.id.frame, fragment3);
-//                transaction.commit();
-//            }
-//        });
     }
 
     private void replaceFragment(Fragment fragment) {
