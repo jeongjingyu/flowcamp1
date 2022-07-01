dc20df2aaa684a96f04f72b17809f1947e4b178e
yeonyeonn <wuvele22@g.skku.edu>
2022-07-01 21:50:45 +0900

    backup

--- app/src/main/java/com/example/flowcamp1/Fragment1.java
+++ app/src/main/java/com/example/flowcamp1/Fragment1.java
@@ -1,8 +1,6 @@
 package com.example.flowcamp1;
 
-import android.content.Intent;
 import android.content.res.AssetManager;
-import android.net.Uri;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -10,16 +8,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
-import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 
 import org.json.JSONArray;
--- app/src/main/java/com/example/flowcamp1/Fragment2.java
+++ app/src/main/java/com/example/flowcamp1/Fragment2.java
@@ -1,23 +1,47 @@
 package com.example.flowcamp1;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.palette.graphics.Palette;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.CompositePageTransformer;
 import androidx.viewpager2.widget.MarginPageTransformer;
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.BitmapImageViewTarget;
+import com.bumptech.glide.request.transition.Transition;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Fragment2 extends Fragment {
+    /*
+    Bitmap bitmap;
+    View viewLightVibrant;
+    TextView tvLightVibrantTitle;
+    TextView tvLightVibrantBody;
+
+    int r;
+    int g;
+    int b;
+    int a;
+    String hexr;
+    String hexg;
+    String hexb;
+    String hexa; */
 
     private ViewPager2 viewPager2;
     private Handler sliderHandler = new Handler();
@@ -61,6 +85,11 @@ public class Fragment2 extends Fragment {
                 super.onPageSelected(position);
                 sliderHandler.removeCallbacks(sliderRunnable);
                 sliderHandler.postDelayed(sliderRunnable, 3000);
+
+                /*bitmap = Bitmap.createScaledBitmap(bitmap, 1, 1, true);
+                Palette palette=Palette.from(bitmap).generate();
+                setPalette(palette);*/
+
             }
         });
 
@@ -86,4 +115,53 @@ public class Fragment2 extends Fragment {
         super.onResume();
         sliderHandler.postDelayed(sliderRunnable, 3000);
     }
+
+    /*
+    private String setPalette(Palette palette) {
+
+        if (palette == null) {
+            return null;
+        }
+
+        // 우리가 선택한 색상표: LightVibrantSwatch
+        Palette.Swatch lightVibrantSwatch = palette.getLightVibrantSwatch();
+        // return 할 hex 형태 컬러 초기화
+        String hex = null;
+
+        if (lightVibrantSwatch != null) {
+
+            // view와 두 텍스트를 이미지의 lightVibrantSwatch한 컬러로 변경
+            viewLightVibrant.setBackgroundColor(lightVibrantSwatch.getRgb());
+            tvLightVibrantTitle.setTextColor(lightVibrantSwatch.getTitleTextColor());
+            tvLightVibrantBody.setTextColor(lightVibrantSwatch.getBodyTextColor());
+            Log.d("lightVibrantSwatch", String.valueOf(lightVibrantSwatch.getRgb()));
+
+            // rgb 값 추출
+            int pixel = lightVibrantSwatch.getRgb(); // -65551 이런 형태
+            r = Color.red(pixel);
+            g = Color.green(pixel);
+            b = Color.blue(pixel);
+            a = Color.alpha(pixel); // alpha: 투명도
+
+            // 위에서 뽑은 r, g, b, a 값을 16진수 hex로 변환
+            hexr = pad(Integer.toHexString(r)); // 아래 pad 함수 정의 (16진수 패딩)
+            hexg = pad(Integer.toHexString(g));
+            hexb = pad(Integer.toHexString(b));
+            hexa = pad(Integer.toHexString(a));
+
+            // 16진수 형태의 r, g, b, a를 통합시켜 "#FF930284" 형태로 변환
+            hex = "#" + hexa + hexr + hexg + hexb;
+            hex = hex.toUpperCase();
+            Log.d("hex color", hex);
+        } // end if
+        return hex;
+    } // end setPalette
+
+    // 16진수 패딩
+    private static final String pad(String s) {
+        return (s.length() == 1) ? "0" + s : s;
+    }*/
 }
+
+
+
