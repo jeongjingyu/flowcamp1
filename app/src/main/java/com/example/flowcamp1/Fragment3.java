85500ab47fb3c8de4cc55cb9e687d417b74e9840
yeonyeonn <wuvele22@g.skku.edu>
2022-07-02 15:44:45 +0900

    fragment3 map - except API key

--- app/src/main/java/com/example/flowcamp1/Fragment3.java
+++ app/src/main/java/com/example/flowcamp1/Fragment3.java
@@ -1,143 +1,158 @@
 package com.example.flowcamp1;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.PointF;
+import android.graphics.drawable.BitmapDrawable;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
-import com.naver.maps.geometry.LatLng;
-import com.naver.maps.map.CameraAnimation;
-import com.naver.maps.map.CameraUpdate;
-import com.naver.maps.map.LocationTrackingMode;
-import com.naver.maps.map.MapView;
-import com.naver.maps.map.NaverMap;
-import com.naver.maps.map.OnMapReadyCallback;
-import com.naver.maps.map.overlay.InfoWindow;
-import com.naver.maps.map.overlay.LocationOverlay;
-import com.naver.maps.map.overlay.Marker;
-import com.naver.maps.map.overlay.Overlay;
-import com.naver.maps.map.overlay.OverlayImage;
-import com.naver.maps.map.util.FusedLocationSource;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Fragment3 extends Fragment implements Overlay.OnClickListener, NaverMap.OnMapClickListener, OnMapReadyCallback {
+import java.io.IOException;
+import java.util.List;
 
-    private MapView mapView;
-    private InfoWindow infoWindow;
-    NaverMap naverMap;
+public class Fragment3 extends Fragment implements OnMapReadyCallback {
+
+    View rootView;
+    com.google.android.gms.maps.MapView mapView;
+    GoogleMap googleMap;
+    String where;
+    double longitude = 127.36583434;
+    double latitude = 36.37421833;
+    String str = "카이스트 IT융합빌딩";
+
 
     public Fragment3() {
 
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment3, container, false);
-        mapView = (MapView)view.findViewById(R.id.map_view);
-        mapView.getMapAsync(this);
+        rootView = inflater.inflate(R.layout.fragment3, container, false);
+        mapView = (com.google.android.gms.maps.MapView) rootView.findViewById(R.id.mapview);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync((com.google.android.gms.maps.OnMapReadyCallback) this);
 
-        return view;
-    }
+        com.google.android.material.textfield.TextInputLayout searchEditText = rootView.findViewById(R.id.searchEditText);
+        Button clickButton = rootView.findViewById(R.id.searchButton);
+        final Geocoder geocoder = new Geocoder(this.getContext());
 
-    @Override
-    public void onMapReady(@NonNull NaverMap naverMap) {
-
-        naverMap.setOnMapClickListener(this);
-
-        infoWindow = new InfoWindow();
-        infoWindow.setAdapter(new InfoWindow.ViewAdapter() {
-            @NonNull
+        clickButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public View getView(@NonNull InfoWindow infoWindow) {
-                return View.inflate(getContext(), R.layout.fragment1, null);
+            public void onClick(View v) {
+                List<Address> list = null;
+                str = searchEditText.getEditText().getText().toString();
+                try {
+                    list = geocoder.getFromLocationName(
+                            str, 10);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                if (list != null) {
+                    if (list.size() == 0) {
+                        Log.v("no", "없단다");
+                    } else {
+                        latitude = list.get(0).getLatitude();
+                        longitude = list.get(0).getLongitude();
+                        com.google.android.gms.maps.CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new com.google.android.gms.maps.model.LatLng(list.get(0).getLatitude(), list.get(0).getLongitude()), 18);
+                        googleMap.animateCamera(cameraUpdate);
+                        googleMap.addMarker(new MarkerOptions()
+                                .position(new com.google.android.gms.maps.model.LatLng(latitude, longitude))
+                                .title(str));
+                    }
+                }
             }
         });
 
-        Marker marker1 = new Marker();
-        marker1.setPosition(new LatLng(36.3624677, 127.358096)); //하바쿡
-        marker1.setMap(naverMap);
-        marker1.setOnClickListener(this);
-
-        Marker marker2 = new Marker();
-        marker2.setPosition(new LatLng(36.3525892, 127.373436)); //와타요업
-        marker2.setMap(naverMap);
-        marker2.setOnClickListener(this);
-
-        Marker marker3 = new Marker();
-        marker3.setPosition(new LatLng(36.3591389, 127.376888)); //토미야
-        marker3.setMap(naverMap);
-        marker3.setOnClickListener(this);
-
-        Marker marker4 = new Marker();
-        marker4.setPosition(new LatLng(36.3512542, 127.373831)); //할머니딸생양곱창
-        marker4.setMap(naverMap);
-        marker4.setOnClickListener(this);
-
-        Marker marker5 = new Marker();
-        marker5.setPosition(new LatLng(36.3574714, 127.350218)); //태평소국밥
-        marker5.setMap(naverMap);
-        marker5.setOnClickListener(this);
-
-        Marker marker6 = new Marker();
-        marker6.setPosition(new LatLng(36.3635414, 127.357655)); //다다카츠
-        marker6.setMap(naverMap);
-        marker6.setOnClickListener(this);
-
-        Marker marker7 = new Marker();
-        marker7.setPosition(new LatLng(36.3620220, 127.353403)); //달구지막창
-        marker7.setMap(naverMap);
-        marker7.setOnClickListener(this);
-
-        Marker marker8 = new Marker();
-        marker8.setPosition(new LatLng(36.3626669, 127.351668)); //주전자
-        marker8.setMap(naverMap);
-        marker8.setOnClickListener(this);
-
-        Marker marker9 = new Marker();
-        marker9.setPosition(new LatLng(36.3621901, 127.351421)); //리코타코
-        marker9.setMap(naverMap);
-        marker9.setOnClickListener(this);
-
-        Marker marker10 = new Marker();
-        marker10.setPosition(new LatLng(36.3636368, 127.358915)); //잇마이타이
-        marker10.setMap(naverMap);
-        marker10.setOnClickListener(this);
+        return rootView;
     }
 
     @Override
-    public void onMapClick(@NonNull PointF pointF, @NonNull LatLng latLng) {
-        if (infoWindow.getMarker() != null) {
-            infoWindow.close();
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        MapsInitializer.initialize(this.getActivity());
+        final Geocoder geocoder = new Geocoder(this.getContext());
+
+        try {
+            List<Address> list = null;
+            str = where;
+            Log.v("str_Cc", str);
+            try {
+                list = geocoder.getFromLocationName(
+                        str, 10);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (list != null) {
+                if (list.size() == 0) {
+                    Log.v("no", "없단다");
+                } else {
+                    latitude = list.get(0).getLatitude();
+                    longitude = list.get(0).getLongitude();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            longitude = 127.36583434;
+            latitude = 36.37421833;
+            str = "카이스트 IT융합빌딩";
         }
-    }
 
-    @Override
-    public boolean onClick(@NonNull Overlay overlay) {
-        Marker marker = (Marker)overlay;
-        if (marker.getInfoWindow() == null) {
-            infoWindow.open(marker);
-            CameraUpdate cameraUpdate = CameraUpdate.scrollTo(infoWindow.getPosition()).animate(CameraAnimation.Easing);
-            naverMap.moveCamera(cameraUpdate);
-        } else {
-            infoWindow.close();
-        }
-        return true;
-    }
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(latitude, longitude), 14);
+        googleMap.animateCamera(cameraUpdate);
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(latitude, longitude))
+                .title(str));
 
-    @Override
-    public void onViewCreated(@NonNull View view,
-                              @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        mapView = view.findViewById(R.id.map_view);
-        mapView.onCreate(savedInstanceState);
+        // 줌 컨트롤 활성화
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+
+        // 마커 추가
+        addStarMarker(36.3624677, 127.358096, "하바쿡");
+        addStarMarker(36.3525892, 127.373436, "와탸요업");
+        addStarMarker(36.3591389, 127.376888, "토미야");
+        addStarMarker(36.3512542, 127.373831, "할머니딸생양곱창");
+        addStarMarker(36.3574714, 127.350218, "태평소국밥");
+        addStarMarker(36.3635414, 127.357655, "다다카츠");
+        addStarMarker(36.3620220, 127.353403, "달구지막창");
+        addStarMarker(36.3626669, 127.351668, "주전자");
+        addStarMarker(36.3621901, 127.351421, "리코타코");
+        addStarMarker(36.3636368, 127.358915, "잇마이타이");
+
     }
 
     @Override
@@ -147,13 +162,13 @@ public class Fragment3 extends Fragment implements Overlay.OnClickListener, Nave
     }
 
     @Override
-    public void onResume() {
+    public void onResume() { //
         super.onResume();
         mapView.onResume();
     }
 
     @Override
-    public void onPause() {
+    public void onPause() { //
         super.onPause();
         mapView.onPause();
     }
@@ -177,8 +192,30 @@ public class Fragment3 extends Fragment implements Overlay.OnClickListener, Nave
     }
 
     @Override
-    public void onLowMemory() {
+    public void onLowMemory() { //
         super.onLowMemory();
         mapView.onLowMemory();
     }
+
+    public void addStarMarker(double latitude, double longitude, String str) {
+        BitmapDrawable bd = (BitmapDrawable) getContext().getResources().getDrawable(R.drawable.star2);
+        Bitmap b = bd.getBitmap();
+        Bitmap starMarker = Bitmap.createScaledBitmap(b, 80, 80, false);
+        googleMap.addMarker(new MarkerOptions().icon(BitmapDescriptorFactory
+                .fromBitmap(starMarker)).position(new LatLng(latitude, longitude)).title(str).snippet("정보창"));
+
+        googleMap.setOnInfoWindowClickListener(infoWindowClickListener);
+    }
+
+    // 정보창 클릭 리스너
+
+    GoogleMap.OnInfoWindowClickListener infoWindowClickListener = new GoogleMap.OnInfoWindowClickListener() {
+        @Override
+        public void onInfoWindowClick(@NonNull Marker marker) {
+            String markerId = marker.getId();
+            Log.d("markerId", markerId);
+        }
+    };
+
+
 }
